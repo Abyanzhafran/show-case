2afdc87ea48846813d435db016e4756ed57b05b2
Abyanzhafran <zhafranjupleks5758@gmail.com>
2023-10-25 15:40:23 +0700

    init commit with updated next js

--- pages/index.js
+++ pages/index.js
@@ -1,236 +1,118 @@
-import MainLayout from '../components/MainLayout';
-import Card from '../components/Card';
-import Head from 'next/head';
-import {
-  BeakerIcon,
-  ShareIcon,
-  CloudUploadIcon,
-  PencilAltIcon,
-  ClipboardListIcon,
-  CashIcon,
-} from '@heroicons/react/solid';
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
 
-const Homepage = () => {
-  const cardPricingItem = [
-    {
-      access: 'Free',
-      price: '$0',
-      caption: 'Always free for 1 user',
-      btn: 'Get started',
-    },
-    {
-      access: 'Standard',
-      price: '$7.50',
-      caption: 'per user (avarage) $75 a month',
-      btn: 'Start trial',
-    },
-    {
-      access: 'Premium',
-      price: '$14.50',
-      caption: 'per user (average) $145 a month',
-      btn: 'Start trial',
-    },
-  ];
-  const cardCompItem = [
-    {
-      icon: <BeakerIcon className="h-14 w-14 text-blue-500" />,
-      title: 'page builder',
-      caption: 'Build your page easily',
-      url: '/pageBuilder',
-    },
-    {
-      icon: <PencilAltIcon className="h-14 w-14 text-blue-500" />,
-      title: 'customized domain',
-      caption: 'Create your beautiful domain',
-      url: '/customizeDomain',
-    },
-    {
-      icon: <ClipboardListIcon className="h-14 w-14 text-blue-500" />,
-      title: 'simple main board',
-      caption: 'Make your own page with very simple main board',
-      url: '/simpleMainBoard',
-    },
-    {
-      icon: <CashIcon className="h-14 w-14 text-blue-500" />,
-      title: 'handle your transaction payment',
-      caption: 'You can handle and customize your own transaction easily',
-      url: '/transaction',
-    },
-    {
-      icon: <CloudUploadIcon className="h-14 w-14 text-blue-500" />,
-      title: 'easy publish page',
-      caption: 'Easy to publish',
-      url: '/easyPublish',
-    },
-    {
-      icon: <ShareIcon className="h-14 w-14 text-blue-500" />,
-      title: 'share and collaborate',
-      caption: 'Share it and collaborate with your friends',
-      url: '/share',
-    },
-  ];
-  const aboutItem = [
-    {
-      name: 'M. Abyan Zhafran Daffa',
-      position: 'Web Developer',
-      img: '/gue_1.png',
-    },
-    {
-      name: 'Ignatius Tegar Saputra',
-      position: 'UI/UX Designer',
-      img: '/tegar_1.png',
-    },
-  ];
+const inter = Inter({ subsets: ['latin'] })
 
+export default function Home() {
   return (
-    <MainLayout>
-      <Head>
-        {/* <title>The page title</title> */}
-        <meta
-          name="google-site-verification"
-          content="edcgayO-Pn_cLPPeIN2-lHU0UJzQuJseBwl4DTTB-pI"
-        />
-      </Head>
-      <main className="w-full">
-        <section
-          id="home"
-          className="flex flex-col items-center justify-center py-8 h-full"
-        >
-          <article className="flex flex-wrap justify-center mt-28 gap-8">
-            <div className="w-64 flex flex-col gap-6">
-              <h1 className="text-5xl font-bold">Showcase</h1>
-              <p className="text-lg">
-                Showcase is web builder that can help people to build their own
-                page for their product and do a payment with it
-              </p>
-            </div>
-            <div>
-              <img src="/dashboard.png" className="w-full h-full lg:h-96" />
-            </div>
-          </article>
-        </section>
-        <section
-          id="features"
-          className="flex flex-col items-center justify-center my-8 h-full"
-        >
-          <div className="py-8 bg-gray-300">
-            <div className="flex flex-col gap-5 px-8 items-center">
-              <div>
-                <span className="font-semibold text-4xl">Features</span>
-                <hr className="border-blue-500 border-2 w-full mt-3 mb-3" />
-              </div>
-              <div className="flex flex-wrap gap-8 justify-center">
-                {cardCompItem.map((item) => (
-                  <Card
-                    className="w-64 sm:w-96 bg-white rounded-lg hover:bg-gray-200"
-                    onClick={() => (window.location.href = item.url)}
-                    key={item.title}
-                  >
-                    <div className="flex flex-col items-center justify-center h-48 gap-3">
-                      {item.icon}
-                      <h2 className="text-xl font-semibold capitalize">
-                        {item.title}
-                      </h2>
-                      <p>{item.caption}</p>
-                      {/* <div className="justify-center card-actions">
-                        <button className="btn btn-outline btn-accent">
-                          Detail Feature
-                        </button>
-                      </div> */}
-                    </div>
-                  </Card>
-                ))}
-              </div>
-            </div>
-          </div>
-        </section>
-        <section id="pricing" className="my-8 h-full">
-          <div className="py-16 px-8 flex flex-col items-center">
-            <div className="mb-8">
-              <span className="font-semibold text-4xl">Pricing</span>
-              <hr className="border-blue-500 border-2 w-full mt-3 mb-3" />
-            </div>
-            <div className="flex flex-wrap items-center justify-center gap-8">
-              {cardPricingItem.map((item) => (
-                <Card className="w-64 sm:w-96 bg-white rounded-lg">
-                  <div className="h-72 flex flex-col items-center justify-start gap-4">
-                    <span className="text-4xl font-bold">{item.access}</span>
-                    <hr className="border-gray-400 border-1 w-full mt-3 mb-3" />
-                    <span className="text-4xl font-bold">{item.price}</span>
-                    <p className="text-lg">{item.caption}</p>
-                    <div className="justify-center card-actions">
-                      <button className="btn btn-outline btn-primary">
-                        {item.btn}
-                      </button>
-                    </div>
-                  </div>
-                </Card>
-              ))}
-            </div>
-          </div>
-        </section>
-        <section id="about" className="my-8 h-full">
-          <div className="py-16 px-8 flex flex-col items-center bg-gray-300">
-            <div className="mb-8">
-              <span className="font-semibold text-4xl">About</span>
-              <hr className="border-blue-500 border-2 w-full mt-3 mb-3" />
-            </div>
-            <div className="flex flex-wrap items-center justify-center gap-8">
-              {aboutItem.map((i) => (
-                <div className="card text-center shadow-2xl bg-white">
-                  <div className="px-10 pt-10">
-                    <img src={i.img} className="rounded-xl" />
-                  </div>
-                  <div className="card-body">
-                    <h2 className="card-title">{i.name}</h2>
-                    <span className="text-lg font-semibold">{i.position}</span>
-                    <div className="justify-center card-actions">
-                      <button className="btn btn-outline btn-accent">
-                        More info
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
-        </section>
-        <section
-          id="contact"
-          className="flex flex-col items-center justify-center py-8 h-full mb-8"
-        >
-          <div className="flex flex-col w-auto sm:w-96 px-10 gap-4">
-            <span className="text-4xl font-semibold font-mono">
-              Get in Touch
-            </span>
-            <div className="form-control">
-              <input
-                type="text"
-                placeholder="Your Name"
-                className="input input-bordered"
-              />
-            </div>
-            <div className="form-control">
-              <input
-                type="text"
-                placeholder="Your Email"
-                className="input input-bordered"
-              />
-            </div>
-            <div className="form-control">
-              <textarea
-                className="textarea h-24 textarea-bordered"
-                placeholder="Send Message"
-              ></textarea>
-            </div>
-            <div className="w-56">
-              <button className="btn btn-primary">Send Message</button>
-            </div>
-          </div>
-        </section>
-      </main>
-    </MainLayout>
-  );
-};
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    >
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Get started by editing&nbsp;
+          <code className="font-mono font-bold">pages/index.js</code>
+        </p>
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          <a
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{' '}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className="dark:invert"
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
+        </div>
+      </div>
 
-export default Homepage;
+      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
+
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Docs{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Find in-depth information about Next.js features and API.
+          </p>
+        </a>
+
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Learn{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          </p>
+        </a>
+
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Templates{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Discover and deploy boilerplate example Next.js&nbsp;projects.
+          </p>
+        </a>
+
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Deploy{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
+        </a>
+      </div>
+    </main>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,18 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [
-    require('daisyui'),
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  daisyui: {
-    themes: false,
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
   },
+  plugins: [],
 }
